3607c4fc8a7bd3676119724c658bf731a2822b51
guptasanil <56546231+guptasanil@users.noreply.github.com>
2020-10-19 18:31:07 +0100

    Update test_lca.py

--- Third Year/Software Engineering/Assignment 1/Python/Lowest Common Ancestor/test_lca.py
+++ Third Year/Software Engineering/Assignment 1/Python/Lowest Common Ancestor/test_lca.py
@@ -3,21 +3,20 @@ import lca
 
 class Testlca(unittest.TestCase):
 
-def test_getlca;
-    f = Node('F', None)
-    a = Node('A', f)
-    c = Node('C', f)
-    h = Node('H', a)
-    e = Node('E', a)
-    i = Node('I', c)
-    b = Node('B', e)
-    t = Node('T', e)
-    d = Node('D', i)
-    self.assertEqual('A', lca.getlca(h, t))
-    self.assertEqual('F', lca.getlca(h, t))
-    self.assertEqual('F', lca.getlca(h, t))
-    self.assertEqual('E', lca.getlca(h, t))
-    self.assertEqual('none', lca.getlca(h, t))
+    def test_getlca(self):
+        f = lca.node('F', None)
+        a = lca.node('A', f)
+        c = lca.node('C', f)
+        h = lca.node('H', a)
+        e = lca.node('E', a)
+        i = lca.node('I', c)
+        b = lca.node('B', e)
+        t = lca.node('T', e)
+        d = lca.node('D', i)
+        self.assertEqual('A', lca.getlca(h, t))
+        self.assertEqual('F', lca.getlca(h, t))
+        self.assertEqual('F', lca.getlca(h, t))
+        self.assertEqual('E', lca.getlca(h, t))
+        self.assertEqual('none', lca.getlca(h, t))
     
-    if __name__ == "__main__":
-    unittest.main()
+    
